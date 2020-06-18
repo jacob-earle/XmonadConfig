2ba40c52169294be9ea0097f0abd2537a1f3e45c
Jacob Earle <jacob.earle@yale.edu>
2020-06-18 16:40:55 -0400

    Add terminal config

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ sudo systemctl enable lightdm
 #install config files
 mkdir $HOME/.xmonad && cp xmonad/xmonad.hs $HOME/.xmonad/xmonad.hs
 cp .zshrc $HOME/.zshrc
+mkdir $HOME/.config
+mkdir $HOME/.config/alacritty && cp alacritty.yml $HOME/.config/alacritty/alacritty.yml
 
 #compile xmonad
 xmonad --recompile
