d8e4cef8264f0a6a3d71e8138732c77327ba7edf
Jacob Earle <jacob.earle@yale.edu>
2020-06-18 21:35:16 -0400

    Fixed typo in setup script

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ mkdir $HOME/.config
 mkdir $HOME/.config/alacritty && cp alacritty.yml $HOME/.config/alacritty/alacritty.yml
 mkdir $HOME/.config/picom && cp picom.conf $HOME/.config/picom.conf
 cp image.jpg $HOME/image.jpg
-cp .xmobarrc $HOME/.xmobar.rc
+cp .xmobarrc $HOME/.xmobarrc
 sudo cp lightdm.conf /etc/lightdm/lightdm.conf
 
 #compile xmonad
