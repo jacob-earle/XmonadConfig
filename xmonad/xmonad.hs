e8ff9686642883463a5247a3d6d03323e1c844d1
Jacob Earle <jacob.earle@yale.edu>
2020-06-18 22:59:43 -0400

    Adding desktop layout to status bar.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,13 +6,17 @@ import XMonad.Util.EZConfig
 import System.IO
 
 main = do
-    xmproc <- spawnPipe "killall feh picom xmobar"
-    xmproc <- spawnPipe "sleep 0.3 && xmobar"
-    xmproc <- spawnPipe "sleep 0.3 && feh --bg-fill $HOME/image.jpg"
-    xmproc <- spawnPipe "sleep 0.3 && picom -b"
+    spawn "killall feh picom"
+    xmproc <- spawnPipe "xmobar"
+    spawn "sleep 0.3 && feh --bg-fill $HOME/image.jpg"
+    spawn "sleep 0.3 && picom -b"
     xmonad $ docks defaultConfig {
     terminal	= "alacritty"
     , modMask	= mod1Mask
     , manageHook= manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , logHook = dynamicLogWithPP xmobarPP
+                    { ppOutput = hPutStrLn xmproc
+                    , ppTitle = xmobarColor "blue" "" . shorten 50
+                    }
     }
